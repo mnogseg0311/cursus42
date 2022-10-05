96ecb2d9ec4e956831343b13943b0b03dd29cb85
Marta Noguera Segura <mnoguera@car12s2.42barcelona.com>
2022-10-05 20:06:35 +0200

    recuperat

---
+++ ft_printf/libftprintf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mnoguera <mnoguera@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/05 19:54:12 by mnoguera          #+#    #+#             */
+/*   Updated: 2022/10/05 20:02:27 by mnoguera         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+# include <stdarg.h>
+# include <unistd.h>
+# include "libft/libft.h"
+
+int	ft_printf(const char *str, ...);
+
+#endif
