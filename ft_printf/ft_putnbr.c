57fca06dd0631056cf2b5d4d22b3f6ab111cc27b
Marta Noguera Segura <mnoguera@car12s2.42barcelona.com>
2022-10-14 18:40:54 +0200

    42

--- ft_printf/ft_putnbr.c
+++ ft_printf/ft_putnbr.c
@@ -6,22 +6,29 @@
 /*   By: mnoguera <mnoguera@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 17:35:33 by mnoguera          #+#    #+#             */
-/*   Updated: 2022/10/13 21:06:01 by mnoguera         ###   ########.fr       */
+/*   Updated: 2022/10/14 18:39:28 by mnoguera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_printf.h"
+#include <stdio.h>
 
 static void	ft_putnum(int n, int *aux, int len)
 {
 	int	i;
+	int	j;
 
 	i = 0;
+	j = 0;
 	if (n > 9)
 		ft_putnum(n / 10, aux, len);
 	if (*aux != -1)
 	{
 		i = n % 10 + 48;
-		*aux += write(1, &i, 1);
+		j += write(1, &i, 1);
+		if (j < 0)
+			*aux = -1;
+		else
+			*aux += j;
 	}
 }
 
@@ -43,12 +50,7 @@ int	ft_putnbr(int n, int len)
 		}
 		ft_putnum(n, &aux, len);
 	}
-	return (aux);
+	if (aux < 0)
+		return (aux);
+	return (aux + len);
 }
-
-#include <stdio.h>
-int	main()
-{
-	printf("%i\n%i\n", printf("%d", -210987), ft_putnbr(-210987, 0));
-}
-
