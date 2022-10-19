380c53a4c6c679db04e99a5d39978c96e94f2b0d
Marta Noguera Segura <mnoguera@car12s2.42barcelona.com>
2022-10-19 14:03:56 +0200

    get next line

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mnoguera <mnoguera@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/19 13:27:52 by mnoguera          #+#    #+#             */
+/*   Updated: 2022/10/19 13:36:52 by mnoguera         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+#include <unistd.h>
+#include <stdlib.h>
+
+char *get_next_line(int fd);
+
+#endif
