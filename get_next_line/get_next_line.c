7bbe6c581f81c13465676bc36818199fa8e0fa5a
Marta Noguera Segura <mnoguera@car12s2.42barcelona.com>
2022-10-21 16:09:18 +0200

    safeness

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,24 +6,70 @@
 /*   By: mnoguera <mnoguera@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 13:26:14 by mnoguera          #+#    #+#             */
-/*   Updated: 2022/10/20 17:44:48 by mnoguera         ###   ########.fr       */
+/*   Updated: 2022/10/21 16:08:16 by mnoguera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line"
+
+int	emptyline(static char *str)
+{
+	char	*aux;
+	int		i;
+	int		j;
+
+	j = strlenvar(str, '\n');
+	aux = strdupvar(str, '\0');
+	if (!aux)
+		return (-1);
+	free(str);
+	str = malloc(sizeof(char) * (strlenvar(aux, '\0') - j));
+	if (!str)
+	{
+		free(aux);
+		return (-1);
+	}
+	j++;
+	i = 0;
+	while (aux[j] != '\0')
+	{
+		str[i] = aux[j];
+		i++;
+		j++;
+	}
+	free(aux);
+	return (1);
+}
+
+int	checkline(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (-1);
+}
 
 int	getdata(static char *str)
 {
 	char	*aux;
 	int		len;
 
-	len = ft_strlen(str);
+	len = strlenvar(str, '\0');
 	aux = ft_strdup(str);
 	if (!aux)
 		return (-1);
 	free(str);
 	str = malloc(sizeof(char) * (len + BUFFER_SIZE));
 	if (!str)
+	{
+		free(aux);
 		return (-1);
+	}
 	str = ft_strcpy(aux);
 	free(aux);
 	aux = malloc(sizeof(char) * BUFFER_SIZE);
@@ -39,9 +85,19 @@ char *get_next_line(int fd)
 	char		*line;
 
 	while (checkline(str) == -1)
-		getdata(str);
+		if (getdata(str) == -1)
+		{
+			free(str);
+			return (NULL);
+		}
 	line = NULL;
-	line = ft_strdup(...);
-	emptyline(str);
+	line = copyline(str);
+	if (!line || emptyline(str) == -1)
+	{
+		if (line)
+			free(line);
+		free(str);
+		return (NULL);
+	}
 	return (line);
 }
