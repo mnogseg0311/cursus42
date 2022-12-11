13503f1cc81a056018ab8f507c212a52e9214ff1
marta <martanogseg@gmail.com>
2022-12-11 11:45:51 +0100

    42

--- push_swap/main.c
+++ push_swap/main.c
@@ -12,6 +12,21 @@
 
 #include "push_swap.h"
 
+#include <stdio.h>
+void	print_pieces(t_stack stackA)
+{
+	t_piece	*piece;
+	t_piece	*aux;
+
+	piece = stackA.first;
+	aux = piece;
+	while (piece->next != NULL)
+	{
+		printf("num=%d\ngame_num=%d\n\n", aux->num, aux->game_num);
+		aux = piece;
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	t_stack	stackA;
@@ -28,6 +43,8 @@ int	main(int argc, char **argv)
 		write(2, "Error\n", 6);
 		return (0);
 	}
-
+//	print_pieces(stackA);
+	printf("%d\n", stackA.len);
+	return (0);
 }
 
