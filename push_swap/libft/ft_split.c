2ad74dad388959d01393e5074957b003cd316dbd
Marta Noguera Segura <mnoguera@car12s2.42barcelona.com>
2023-03-10 18:39:10 +0100

    11

--- push_swap/libft/ft_split.c
+++ push_swap/libft/ft_split.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mnoguera <mnoguera@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/26 15:43:07 by mnoguera          #+#    #+#             */
-/*   Updated: 2022/09/27 15:42:03 by mnoguera         ###   ########.fr       */
+/*   Created: 2023/03/08 16:03:35 by mnoguera          #+#    #+#             */
+/*   Updated: 2023/03/10 18:38:18 by mnoguera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,54 +23,24 @@ static void	ft_free_malloc(char **mat, int i)
 	free(mat);
 }
 
-static int	ft_fillaux(char const *s, char c, char **mat, int v[])
-{
-	while (v[3] < v[0])
-	{
-		if (s[v[1]] == c)
-			while (s[v[1]] == c)
-				v[1]++;
-		else
-		{
-			v[2] = v[1];
-			v[4] = 0;
-			while (s[v[1]] != c && s[v[1]] != '\0')
-			{
-				v[1]++;
-				v[4]++;
-			}
-			mat[v[3]] = ft_substr(s, v[2], v[4]);
-			if (!mat[v[3]])
-			{
-				ft_free_malloc(mat, v[3]);
-				return (0);
-			}
-			v[3]++;
-		}
-	}
-	mat[v[3]] = NULL;
-	return (1);
-}
+
 
 static int	ft_fillmat(char const *s, char c, char **mat, int words)
 {
 	int	i;
-	int	aux;
-	int	j;
-	int	len;
-	int	vect[5];
 
-	aux = 0;
-	j = 0;
 	i = 0;
-	len = 0;
-	vect[0] = words;
-	vect[1] = i;
-	vect[2] = aux;
-	vect[3] = j;
-	vect[4] = len;
-	if (!ft_fillaux(s, c, mat, vect))
-		return (0);
+	while (i < words)
+	{
+		mat[i] = get_word(s, c, i);
+		if (!mat[i])
+		{
+			ft_free_malloc(mat, i);
+			return (0);
+		}
+		i++;
+	}
+	mat[i] = NULL;
 	return (1);
 }
 
@@ -98,7 +68,7 @@ static int	ft_words(char const *s, char c)
 	return (num);
 }
 
-char	**ft_split(char const *s, char c)
+char	**ft_split(char const *s, char *c)
 {
 	char	**mat;
 	int		words;
